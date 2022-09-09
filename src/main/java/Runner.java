16b9b982058f5ba461c195074d05aa077849c05f
Kat Fagg <katjfagg@gmail.com>
2022-09-09 12:14:42 +0100

    pull request process 1

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -3,6 +3,7 @@ public class Runner {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Develop branch");
+        System.out.println("We're making a pull request");
     }
 
 }
